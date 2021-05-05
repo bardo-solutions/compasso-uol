678213d7167b8cf8927c24c756e75c2b7b5c4df4
Daniel Piassi <dandampi@gmail.com>
2021-05-05 18:47:57 -0300

    comments added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Tooltip("Target transform.")]
     [SerializeField] Transform m_Player;
-    [SerializeField] float m_Speed = 100f;
+
+    [Tooltip("Duration of each step animation in seconds.")]
+    [SerializeField] float m_MoveTime = 1f;
+
+    [Tooltip("Delay time between each step in seconds.")]
     [SerializeField] float m_DelayTime = 0.5f;
 
-    readonly List<Vector3> m_Buffer = new List<Vector3>();
+    // Auxiliar members.
+    readonly List<Vector3> m_DisplacementBuffer = new List<Vector3>();
     bool m_IsRunning = false;
 
     public void OnKeyboardEvent(KeyboardEvent t)
     {
         if (m_IsRunning)
         {
+            // Discard inputs while animation is running.
             return;
         }
 
@@ -24,54 +31,60 @@ public class PlayerController : MonoBehaviour
                 StartCoroutine(Run());
                 break;
             case KeyboardEvent.Up:
-                m_Buffer.Add(Vector3.forward);
+                m_DisplacementBuffer.Add(Vector3.forward);
                 break;
             case KeyboardEvent.Down:
-                m_Buffer.Add(Vector3.back);
+                m_DisplacementBuffer.Add(Vector3.back);
                 break;
             case KeyboardEvent.Left:
-                m_Buffer.Add(Vector3.left);
+                m_DisplacementBuffer.Add(Vector3.left);
                 break;
             case KeyboardEvent.Right:
-                m_Buffer.Add(Vector3.right);
+                m_DisplacementBuffer.Add(Vector3.right);
                 break;
             default:
                 break;
         }
     }
 
-
     IEnumerator Run()
     {
         if (m_IsRunning)
         {
+            // Prevent coroutine overflow.
             yield break;
         }
 
         // Activate flag.
         m_IsRunning = true;
 
-        foreach (var displacement in m_Buffer)
+        foreach (var displacement in m_DisplacementBuffer)
         {
+            // Iterate over displacement buffer.
             var from = m_Player.position;
             var to = from + displacement;
+
+            // Wait for coroutine finishes before continue.
             yield return StartCoroutine(LerpMove(from, to));
+
+            // Wait a little between each iteration.
             yield return new WaitForSeconds(m_DelayTime);
         }
 
         // Reset buffer.
-        m_Buffer.Clear();
+        m_DisplacementBuffer.Clear();
 
         // Disable flag.
         m_IsRunning = false;
     }
 
+    // TODO turn it into a custom Tweening library.
     IEnumerator LerpMove(Vector3 source, Vector3 target)
     {
         for (int i = 1; i <= 100; i++)
         {
             m_Player.position = Vector3.Lerp(source, target, i / 100f);
-            yield return new WaitForSeconds(1f / m_Speed);
+            yield return new WaitForSeconds(1f / m_MoveTime);
         }
     }
 }
